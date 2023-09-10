6c01935f1d1209f7bc0b015532982d94aee7ead9
Kthmin <68198657+Kithmin@users.noreply.github.com>
2023-09-10 14:55:33 +0530

    push

---
+++ src/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Sidebar.css'
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+function Sidebar() {
+  return (
+    <div className='Sidebar'>
+       
+        {/*Twitter icon*/}
+        <TwitterIcon/>
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+        {/*SidebarOption*/}
+
+    
+
+    </div>
+    
+  )
+}
+
+export default Sidebar
